2e911bf6cb554f1c108267fc29e1da86dd6c7db0
Adam Taufmann <ataufmann@gmail.com>
2017-03-15 14:47:10 -1000

    Finished blog comments feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'articles#index'			#This roots http://localhost:3000 to the 'Articles' index page
-  resources :articles 				#so you don't have to type in http://localhost:3000/articles
+  root to: 'articles#index'			#This roots http://localhost:3000 to the 'Articles' index page so you don't have to type in http://localhost:3000/articles
+  resources :articles do
+  	resources :comments
+  end
 end
